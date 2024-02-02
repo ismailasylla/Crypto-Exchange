c5d8c775cafabad46555a5f0b0e3b028a83d9cf8
Sylla <ismailasyllaa@gmail.com>
2024-02-03 01:40:48 +0400

    add footer layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Layout } from 'antd';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Layout, Space, Typography } from 'antd';
 import { Cryptocurrencies, CrytoDetails, Exchanges, HomePage, Navbar, News } from './components';
 
 const App = () => {
@@ -22,8 +22,18 @@ const App = () => {
             </Routes>
           </div>
         </Layout>
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Crypto Exchange <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchange">Exchange</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className='footer'></div>
     </div>
   );
 };
