c0086ba8a9db2efc5dc8436e411d81325d20f992
Sylla <ismailasyllaa@gmail.com>
2024-02-03 01:29:21 +0400

    add component arch & routing functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd'
-import { Navbar } from './components';
-
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+import { Cryptocurrencies, CrytoDetails, Exchanges, HomePage, Navbar, News } from './components';
 
 const App = () => {
   return (
@@ -12,13 +11,21 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/cryto/:coinId" element={<CrytoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
       </div>
-      <div className='footer'>
-
-      </div>
+      <div className='footer'></div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
