80c719ef086c78feb745b44ba0a021d47071e7e4
Sylla <ismailasyllaa@gmail.com>
2024-02-02 22:46:05 +0400

    Implement initial UI navigation structure with React Router and basic route components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd'
+import { Navbar } from './components';
+
 
 const App = () => {
   return (
-    <div>Crypto Exchange</div>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
   )
 }
 
