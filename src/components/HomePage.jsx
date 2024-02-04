54863d3437dc1c671fe0429c75abd428a998077d
Sylla <ismailasyllaa@gmail.com>
2024-02-04 16:53:21 +0400

    Add sections for top cryptos and crypto news on home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,9 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import Loader from './Loader';
+import { Link } from 'react-router-dom';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
 
 const { Title } = Typography;
 
@@ -51,6 +54,24 @@ const Homepage = () => {
           />
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptos In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3}>
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 };
