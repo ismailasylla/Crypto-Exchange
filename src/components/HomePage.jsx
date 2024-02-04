a8d83b410a85024fa45c7bd612408736c32cc6b6
Sylla <ismailasyllaa@gmail.com>
2024-02-04 16:43:42 +0400

    Fetch data statistics for HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,32 +1,58 @@
-import { Col, Row, Statistic, Typography } from 'antd';
 import React from 'react';
+import millify from 'millify';
+import { Typography, Row, Col, Statistic } from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
 
 const { Title } = Typography;
-const HomePage = () => {
+
+const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
+
+  if (isFetching) return <Loader />;
+
+  console.log(data);
+
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
-      <Row>
+      <Row gutter={[32, 32]}>
         <Col span={12}>
-          <Statistic title="Total Crypto Currencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic
+            title="Total Market Cap:"
+            value={`$${millify(globalStats.totalMarketCap)}`}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total 24h Volume"
+            value={`$${millify(globalStats.total24hVolume)}`}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
     </>
   );
 };
 
-export default HomePage;
+export default Homepage;
